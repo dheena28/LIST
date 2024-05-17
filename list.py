73c41fecda2e268484bca64a888c1b15c5bfca16
Dheena <140698317+dheena28@users.noreply.github.com>
2024-05-17 20:33:43 +0530

    initial commit

---
+++ list.py
@@ -0,0 +1,8 @@
+d=[23,24,25,26,27]
+print("List value before change",d)
+print("Enter the INDEX")
+i= int(input())
+print("Enter the Number")
+n= int(input())
+d[i]=n
+print("list value after change",d)
